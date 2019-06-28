e89e8489363e055316bac9c54fe1c823b9bf1ee6
josemak25 <amakirij@gmail.com>
2019-06-28 21:50:40 +0100

    add route to pages

--- client/src/components/Router.js
+++ client/src/components/Router.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Driver from './Driver';
+import Dashboard from './Dashboard';
 
 const Navigator = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={Driver} />
+        <Route exact path="/dashboard" component={Dashboard} />
       </Switch>
     </BrowserRouter>
   );
